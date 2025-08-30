ce1551da87c05332e3a8c4430e957f954cffb9a7
Thirasit Thothong <94747321+KATANA0x00@users.noreply.github.com>
2025-08-30 09:16:08 +0700

    Add files via upload

--- Watermark_LoRa_ESP32H2_LIGHT_CONTROL.ino
+++ Watermark_LoRa_ESP32H2_LIGHT_CONTROL.ino
@@ -7,39 +7,68 @@
 
 #define HOLD 3
 
-const LoRaWANBand_t Region = AS923;
+const LoRaWANBand_t Region = AS923; // Set Frequency to 923 MHz
 
-uint32_t DevAddr = 0x3EDE35CB;
-const char NwkSKey[] = "FF719E06EAE64692568821CD3FDBB131";
-const char AppSKey[] = "495FE93D882E7C0D259471505119FB6D";
+uint32_t DevAddr = 0x6E395B2B;
+const char NwkSKey[] = "89A9A66A61EBB9D1895E1C240EF0702A";
+const char AppSKey[] = "63B2F3FB4A78EBE18CD761D2493E6609";
 
 SX1276 radio = new Module(RFM95_CS, RFM95_INT, RFM95_RST);
 LoRaWANNode node(&radio, &Region);
 
 char messageBuffer[64];
-int LORA_SEND_TIME = 10;
+int LORA_SEND_TIME = 10; // Set Sleep Time to 10 sec -> 10 * 1000 * 1000 us
+/*
+Init Board
+  |
+  V
+RGB -> White
+  |
+  V
+Send Uplink Sensor Data
+  |
+  V
+Wait for Downlink
+  |
+  V
+If have downlink -> Set RGB to Blue, Set ON - OFF Light
+  |
+  V
+RGB -> OFF
+  |
+  V
+Sleep
+*/
 void setup() {
   SPI.begin(10, 4, 5);
   pinInit();
-  rgbLedWrite(NEO_PIXEL, 100, 100, 100);
+  rgbLedWrite(NEO_PIXEL, 100, 100, 100); 
   pinMode(HOLD, OUTPUT);
 
   while (radio.begin());
   radio.setSpreadingFactor(10);
   radio.setBandwidth(125.0);
 
-  node.beginABP(DevAddr, NULL, NULL, hexStringToBytes(NwkSKey).data(), hexStringToBytes(AppSKey).data());
+  node.beginABP(DevAddr, NULL, NULL, hexStringToBytes(NwkSKey).data(), hexStringToBytes(AppSKey).data());             // Set Key for Node
   node.activateABP();
   node.setDeviceStatus(map(getBattery(), 0, 100, 0, 255));
-  snprintf(messageBuffer, sizeof(messageBuffer), "{\"moisture\":%.2f,\"BATT\":%.2f}", getSensor(), getBattery());
+  snprintf(messageBuffer, sizeof(messageBuffer), "{\"moisture\":%.2f,\"BATT\":%.2f}", getSensor(true), getBattery()); // Create Message
+  -----------------------
+  getSensor() - read WaterMark sensor with Raw ADC
+  getSensor(true) - read WaterMark sensor in Resistance
+
+  getBatter() - read Battery level with Raw ADC
+  getBatter(true) - read Battery level in Resistance
+  -----------------------
+  */
   const char* message = messageBuffer;
   uint8_t downlinkBuffer[64];
   size_t downlinkLength = 0;
-  int16_t state = node.sendReceive(message, 1, downlinkBuffer, &downlinkLength, false, NULL, NULL);
+  int16_t state = node.sendReceive(message, 1, downlinkBuffer, &downlinkLength, false, NULL, NULL);                   // Send Uplink and Wait for Downlink
 
   if (downlinkLength > 0) {
-    rgbLedWrite(NEO_PIXEL, 0, 100, 100);
-    int result = HexToStr(downlinkBuffer, downlinkLength).toInt();
+    rgbLedWrite(NEO_PIXEL, 0, 100, 100);                                                                              // If Downlink
+    int result = HexToStr(downlinkBuffer, downlinkLength).toInt();                                                    // Convert Downlink to Number
     gpio_hold_dis((gpio_num_t)HOLD);
     digitalWrite(HOLD, result);
     gpio_hold_en((gpio_num_t)HOLD);
@@ -49,7 +78,7 @@ void setup() {
 
   delay(500);
   rgbLedWrite(NEO_PIXEL, 0, 0, 0);
-  esp_sleep_enable_timer_wakeup(LORA_SEND_TIME * 1000 * 1000);
+  esp_sleep_enable_timer_wakeup(LORA_SEND_TIME * 1000 * 1000);                                                        // Sleep
   esp_deep_sleep_start();
 }
 
